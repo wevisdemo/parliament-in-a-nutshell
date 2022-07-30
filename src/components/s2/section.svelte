89c98c55ec143aa060ce1618b230480457f9d3e7
rootEnginear <rootEnginear@users.noreply.github.com>
2022-07-31 05:56:38 +0700

    feat: add S2
    S2 overall number

---
+++ src/components/s2/section.svelte
@@ -0,0 +1,90 @@
+<!-- SECTION 2 - OVERALL NUMBER -->
+<script lang="ts">
+	// @ts-nocheck
+	import { onMount } from 'svelte';
+	import { inView, scroll, animate, ScrollOffset } from 'motion';
+
+	import ARC from 'components/s2/arc.svelte';
+
+	let show_current_data = false;
+
+	let el_part2_trigger;
+	let el_h1;
+	let el_h2;
+
+	onMount(() => {
+		inView(el_part2_trigger, () => {
+			show_current_data = true;
+
+			return () => (show_current_data = false);
+		});
+
+		scroll(
+			animate(el_h1, {
+				opacity: [1, 1, 0, 0]
+			}),
+			{
+				target: el_h1,
+				offset: ScrollOffset.Exit
+			}
+		);
+
+		scroll(
+			animate(el_h2, {
+				opacity: [0, 1]
+			}),
+			{
+				target: el_h1,
+				offset: ScrollOffset.Exit
+			}
+		);
+	});
+</script>
+
+<section>
+	<div class="stick" style="--h:120vh">
+		<ARC {show_current_data} />
+	</div>
+	<div class="floating">
+		<h1 bind:this={el_h1} class="T1">
+			ณ ปีสุดท้าย ของสมัยสภาผู้แทนราษฏรชุดที่ 25<br />
+			<span class="bigger">มี ส.ส. ปฏิบัติหน้าที่อยู่ทั้งสิ้น ...</span>
+		</h1>
+		<h1 bind:this={el_h2} class="T1">
+			ณ ปีสุดท้าย ของสมัยสภาผู้แทนราษฏรชุดที่ 25<br />
+			<span class="bigger">มี ส.ส. ปฏิบัติหน้าที่อยู่ทั้งสิ้น ...</span>
+		</h1>
+	</div>
+	<div bind:this={el_part2_trigger} style="--h:40vh" />
+</section>
+
+<style lang="scss">
+	.stick {
+		position: sticky;
+		top: 0;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+
+		z-index: 1;
+	}
+
+	.floating {
+		position: absolute;
+		inset: 0;
+	}
+
+	h1 {
+		width: 100%;
+		text-align: center;
+		padding-top: 17vh;
+		margin: 0;
+	}
+
+	.bigger {
+		font-size: 1.5em;
+		line-height: 1.6;
+	}
+</style>
