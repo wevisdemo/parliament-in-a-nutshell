e06d738c9bdb42a6ffab6e63b0bbfa18a6226652
rootEnginear <rootEnginear@users.noreply.github.com>
2022-08-08 00:39:47 +0700

    fix: rename S34 to S3S4
    avoid confusion if this is section "34" or "3 and 4"

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,11 +2,11 @@
 	import 'styles/master.scss';
 	import S1 from 'components/s1/s1.svelte';
 	import S2 from 'components/s2/s2.svelte';
-	import S34 from 'components/s34/s34.svelte';
+	import S3S4 from 'components/s3s4/s3s4.svelte';
 	import S5S6 from 'components/s5s6/s5s6.svelte';
 </script>
 
 <S1 />
 <S2 />
-<S34 />
+<S3S4 />
 <S5S6 />
