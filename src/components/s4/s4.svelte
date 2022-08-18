c6fd052cd8f30b4b80b1a8eecfe72f03dde93449
rootEnginear <rootEnginear@users.noreply.github.com>
2022-08-18 15:45:20 +0700

    fix(S3): font size

--- src/components/s4/s4.svelte
+++ src/components/s4/s4.svelte
@@ -12,8 +12,8 @@
 		background: linear-gradient(#fff0, #fff0 25%, #000f 90%);
 
 		> .T1 {
-			font-size: 3.5rem;
-			line-height: 1.5;
+			font-size: 3rem;
+			line-height: 1.3;
 			mix-blend-mode: difference;
 			color: white;
 		}
