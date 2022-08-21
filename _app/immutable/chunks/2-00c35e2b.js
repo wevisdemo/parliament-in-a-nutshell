58b69d68bf1134bb90458ff383bed0220403be49
rootEnginear <suthep.chanchuphol@outlook.com>
2022-08-21 18:11:29 +0000

    Update gh-page to output generated at 86857cd

---
+++ _app/immutable/chunks/2-00c35e2b.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/pages/_page.svelte-32be149d.js";import"./index-999d5a79.js";import"./index-f8fae127.js";export{r as component};
--- _app/immutable/chunks/2-8aabe5ed.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/pages/_page.svelte-5cd167c9.js";import"./index-999d5a79.js";import"./index-f8fae127.js";export{r as component};
