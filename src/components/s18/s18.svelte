7dc8b11a448fed71c3c1dc86a881111f4f6a32c6
rootEnginear <rootEnginear@users.noreply.github.com>
2022-08-22 19:53:11 +0700

    fix(S18): add shadow

--- src/components/s18/s18.svelte
+++ src/components/s18/s18.svelte
@@ -109,8 +109,8 @@
 	.ref-container {
 		padding: 192px 0 144px;
 		z-index: 3;
-		// box-shadow: 0 24px 38px 3px rgb(0 0 0 / 14%), 0 9px 46px 8px rgb(0 0 0 / 12%),
-		// 	0 11px 15px -7px rgb(0 0 0 / 20%);
+		box-shadow: 0 24px 38px 3px rgb(0 0 0 / 14%), 0 9px 46px 8px rgb(0 0 0 / 12%),
+			0 11px 15px -7px rgb(0 0 0 / 20%);
 	}
 
 	.T1 {
