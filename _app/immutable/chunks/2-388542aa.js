72d3efc3b9c7f7d8245f8286c42cb7b4cee16ce7
rootEnginear <suthep.chanchuphol@outlook.com>
2022-08-24 16:36:35 +0000

    Update gh-page to output generated at e48c872

---
+++ _app/immutable/chunks/2-388542aa.js
@@ -0,0 +1,1 @@
+import{default as r}from"../components/pages/_page.svelte-8f1a6039.js";import"./index-95797fcc.js";import"./index-e8e90839.js";export{r as component};
--- _app/immutable/chunks/2-d5ad181f.js
+++
@@ -1,1 +0,0 @@
-import{default as r}from"../components/pages/_page.svelte-e7c99e12.js";import"./index-95797fcc.js";import"./index-e8e90839.js";export{r as component};
